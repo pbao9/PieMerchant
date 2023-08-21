dea864000bf0bd748eb2f38c438737ef02049931
b4ro711 <baobaoo5533@gmail.com>
2023-08-21 21:34:49 +0700

    refesh

--- app/src/main/java/com/pbao/piemerchant/MainActivity.java
+++ app/src/main/java/com/pbao/piemerchant/MainActivity.java
@@ -8,18 +8,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtTextView = findViewById(R.id.txtTextView);
 
-        txtTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Vui lòng nhấn lại!", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
